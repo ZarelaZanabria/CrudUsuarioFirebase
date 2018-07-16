51a8527c36aba19e1c592be22cfc18324a0f38ac
ZarelixCoder <38471200+ZarelixCoder@users.noreply.github.com>
2018-07-16 13:14:43 -0500

    Boton Update
    Boton para Update

--- js/usuario.js
+++ js/usuario.js
@@ -24,7 +24,7 @@ function inicializar() {
 
 const showDataUsers = () => {
     //Quiero que cada que este en value , me hague algo
-    
+
     refUsers.on('value', function (snap) {
         //Vamos a obtener los valores de la base de datos Ususer
         let dataUsers = snap.val();
@@ -39,28 +39,42 @@ const showDataUsers = () => {
                 '<td>' + dataUsers[dataUser].usersLastName + '</td>' +
                 '<td>' + dataUsers[dataUser].usersEmail + '</td>' +
                 '<td>' + dataUsers[dataUser].usersPassword + '</td>' +
-                '<td></td>' +
                 '<td>' +
-                '<button class ="btn btn-danger delete" data-users="' + dataUser + '">'+
-                     '<span class = "glyphicon glyphicon-trash"></span>' + 
+                '<button class ="btn btn-danger update" data-users="' + dataUser + '">' +
+                '<span class = "glyphicon glyphicon-pencil"></span>' +
                 '</button>' +
-                '</td>' + 
+                '</td>' +
+                '<td>' +
+                '<button class ="btn btn-danger delete" data-users="' + dataUser + '">' +
+                '<span class = "glyphicon glyphicon-trash"></span>' +
+                '</button>' +
+                '</td>' +
                 '</tr>';
         }
 
         tbodyTableUsers.innerHTML = toRows;
         //Para poder borrar hacemos una condicion si la tabla tiene filas
-        if (toRows!= "") {
+        if (toRows != "") {
+
             //Elemento elementDeleteUser que contiene todos los elemtos de la clase borrar
-           let elementDeleteUser = document.getElementsByClassName('delete');
-           //de la cantidad de elementos borrables 
+            let elementUpdateUser = document.getElementsByClassName('update');
+            //de la cantidad de elementos borrables 
+            for (let index = 0; index < elementUpdateUser.length; index++) {
+                //lo que hacemos es crear un evento que cuando hacemos click en algunos
+                // de los botones entonces tendremos que borrar.
+                elementUpdateUser[index].addEventListener('click', updateUsers, false);
+
+            }
+            //Elemento elementDeleteUser que contiene todos los elemtos de la clase borrar
+            let elementDeleteUser = document.getElementsByClassName('delete');
+            //de la cantidad de elementos borrables 
             for (let index = 0; index < elementDeleteUser.length; index++) {
                 //lo que hacemos es crear un evento que cuando hacemos click en algunos
                 // de los botones entonces tendremos que borrar.
-                elementDeleteUser[index].addEventListener('click',deleteUsers,false);
-                
+                elementDeleteUser[index].addEventListener('click', deleteUsers, false);
+
             }
-            
+
         }
 
     });
@@ -68,12 +82,28 @@ const showDataUsers = () => {
 }
 
 function deleteUsers() {
-    
+
     //Quiero coger aquel elemento que yo hiz click quiero borrar
     let dataUserDelete = this.getAttribute("data-users");
     let refUsersDelete = refUsers.child(dataUserDelete);
     refUsersDelete.remove();
-    
+
+}
+
+function updateUsers() {
+    //Quiero coger aquel elemento que yo hiz click quiero borrar
+    let dataUserUpdate = this.getAttribute("data-users");
+    let refUsersUpdate = refUsers.child(dataUserUpdate);
+    refUsersUpdate.once('value',function (snap) {
+        var datos = snap.val();
+        document.getElementById('users-name').value=datos.usersEmail;
+        document.getElementById('users-last-name').value=datos.usersLastName;
+        document.getElementById('users-email').value=datos.usersName;
+        document.getElementById('users-password').value=datos.usersPassword;
+        
+    })
+    document.getElementById('button-update')
+
 }
 
 
@@ -89,5 +119,7 @@ const submitUsersFirebase = (event) => {
     //Para borrar una vez que guarda los archivos
     formUsers.reset();
 }
+
+
 
 
